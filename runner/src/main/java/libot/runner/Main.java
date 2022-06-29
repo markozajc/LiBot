16392bd9a132d9fddcd1ec21f483ac5937289744
Marko Zajc <marko@zajc.eu.org>
2022-06-30 01:11:29 +0200

    [runner] Add runner

---
+++ runner/src/main/java/libot/runner/Main.java
@@ -0,0 +1,14 @@
+package libot.runner;
+
+public class Main {
+
+	// Due to how Eclipse handles Maven projects, and because I can't simply import
+	// modules with runtime scope in libot-core (it would still cause a cyclic dependency
+	// in Maven), a top-level project importing all projects with runtime scope (except
+	// libot-core, which the main function calls) is required for execution from within
+	// the IDE itself. Deal with it.
+	public static void main(String[] argv) throws Exception {
+		libot.core.Main.main(argv);
+	}
+
+}
