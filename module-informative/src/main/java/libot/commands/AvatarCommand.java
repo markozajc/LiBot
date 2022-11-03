2a889822b0a9374547067b572c7805eb9a9eeb85
Marko Zajc <marko@zajc.eu.org>
2022-11-03 17:33:28 +0100

    [module-informative] Avatar command can now fetch avatars based on user ID

--- module-informative/src/main/java/libot/commands/AvatarCommand.java
+++ module-informative/src/main/java/libot/commands/AvatarCommand.java
@@ -1,22 +1,49 @@
-package libot.commands;
+package libot.command;
 
 import static libot.core.Constants.LITHIUM;
-import static libot.core.commands.CommandCategory.INFORMATIVE;
-import static libot.utils.Utilities.array;
+import static libot.core.command.CommandCategory.INFORMATIVE;
+import static libot.util.CommandUtils.findUserOrAuthor;
+import static libot.util.ParseUtils.parseLong;
+import static libot.util.Utilities.array;
+import static net.dv8tion.jda.api.requests.ErrorResponse.UNKNOWN_USER;
+import static net.dv8tion.jda.internal.requests.RestActionImpl.getDefaultFailure;
 
-import libot.core.commands.*;
+import javax.annotation.Nonnull;
+
+import libot.core.command.*;
 import libot.core.entities.CommandContext;
-import libot.core.extensions.EmbedPrebuilder;
-import libot.utils.CommandUtils;
+import libot.core.extension.EmbedPrebuilder;
+import net.dv8tion.jda.api.entities.User;
+import net.dv8tion.jda.api.exceptions.ErrorResponseException;
 
 public class AvatarCommand extends Command {
 
 	@Override
+	@SuppressWarnings("null")
 	public void execute(CommandContext c) {
-		var target = CommandUtils.findUserOrAuthor(c);
+		if (c.getMessage().getMentionedUsers().isEmpty() && c.params().check(1)) {
+			long id = parseLong(c.params().get(0));
+			var user = c.shredder().getUserById(id);
+			if (user != null) {
+				sendAvatar(c, user);
+			} else {
+				c.jda().retrieveUserById(id).queue(u -> sendAvatar(c, u), e -> {
+					if (e instanceof ErrorResponseException ere && ere.getErrorResponse() == UNKNOWN_USER)
+						c.reply("Couldn't find a user with that ID.");
+					else
+						getDefaultFailure().accept(e);
+				});
+			}
+
+		} else {
+			sendAvatar(c, findUserOrAuthor(c));
+		}
+	}
+
+	private static void sendAvatar(@Nonnull CommandContext c, @Nonnull User user) {
 		var e = new EmbedPrebuilder(LITHIUM);
-		e.setImage(target.getEffectiveAvatarUrl());
-		e.setDescriptionf("[Download](%s)", target.getEffectiveAvatarUrl());
+		e.setImage(user.getEffectiveAvatarUrl());
+		e.setDescriptionf("[Download](%s)", user.getEffectiveAvatarUrl());
 		c.reply(e);
 	}
 
@@ -29,12 +56,12 @@ public class AvatarCommand extends Command {
 	public String getInfo() {
 		return """
 			Displays a link to the mentioned user's avatar (profile picture). \
-			If no user is mentioned, your avatar will be displayed.""";
+			If no user is mentioned and no user ID is provided, your avatar will be displayed.""";
 	}
 
 	@Override
 	public String[] getParameters() {
-		return array("[user]");
+		return array("[user or ID]");
 	}
 
 	@Override
