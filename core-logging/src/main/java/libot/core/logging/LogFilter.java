3ae24f8bfa8f4e0094a9e83691d2c1c5f5663058
Marko Zajc <marko@zajc.eu.org>
2023-04-10 19:18:49 +0200

    [core-logging] Filter more events in the log

--- core-logging/src/main/java/libot/core/logging/LogFilter.java
+++ core-logging/src/main/java/libot/core/logging/LogFilter.java
@@ -6,7 +6,9 @@ import static ch.qos.logback.core.spi.FilterReply.*;
 import org.reflections.Reflections;
 
 import com.sedmelluq.discord.lavaplayer.container.ogg.OggContainerProbe;
+import com.sedmelluq.discord.lavaplayer.source.youtube.YoutubeAccessTokenTracker;
 import com.sedmelluq.discord.lavaplayer.track.playback.LocalAudioTrackExecutor;
+import com.sedmelluq.lava.common.natives.NativeLibraryLoader;
 
 import ch.qos.logback.classic.Level;
 import ch.qos.logback.classic.spi.ILoggingEvent;
@@ -17,35 +19,46 @@ import net.dv8tion.jda.internal.requests.WebSocketClient;
 
 public class LogFilter extends Filter<ILoggingEvent> {
 
-	private static final String OGG_CONTAINER_PROBE = OggContainerProbe.class.getName();
-	private static final String LATE = LocalAudioTrackExecutor.class.getName();
-	private static final String WS_CLIENT = WebSocketClient.class.getName();
-	private static final String REFLECTIONS = Reflections.class.getName();
-	private static final String JDA = JDA.class.getName();
+	private static final String LOG_OGG_CONTAINER_PROBE = OggContainerProbe.class.getName();
+	private static final String LOG_LATE = LocalAudioTrackExecutor.class.getName();
+	private static final String LOG_WS_CLIENT = WebSocketClient.class.getName();
+	private static final String LOG_REFLECTIONS = Reflections.class.getName();
+	private static final String LOG_JDA = JDA.class.getName();
+	private static final String LOG_YATT = YoutubeAccessTokenTracker.class.getName();
+	private static final String LOG_NLL = NativeLibraryLoader.class.getName();
 
-	private static final String MESSAGE_CHUNK_ERROR = "Encountered exception trying to handle member chunk response";
-	private static final String MESSAGE_CANT_PARSE_OGG = "Failed to collect additional information on OGG stream.";
-	private static final String MESSAGE_PLAYBACK_ERROR = "Error in playback of ";
-	private static final String MESSAGE_REFLECTIONS_TOOK = "Reflections took ";
-	private static final String MESSAGE_CONNECTED = "Connected to WebSocket";
-	private static final String MESSAGE_LOADED = "Finished Loading!";
-	private static final String MESSAGE_LOGIN = "Login Successful!";
+	private static final String MSG_CHUNK_ERROR = "Encountered exception trying to handle member chunk response";
+	private static final String MSG_CANT_PARSE_OGG = "Failed to collect additional information on OGG stream.";
+	private static final String MSG_PLAYBACK_ERROR = "Error in playback of ";
+	private static final String MSG_REFLECTIONS_TOOK = "Reflections took ";
+	private static final String MSG_CONNECTED = "Connected to WebSocket";
+	private static final String MSG_LOADED = "Finished Loading!";
+	private static final String MSG_LOGIN = "Login Successful!";
+	private static final String MSG_UPDATING_YAT = "Updating YouTube visitor id";
+	private static final String MSG_LOADING_LIBMPG = "Native library libmpg123-0: loading with filter";
+	private static final String MSG_LOADING_CONNECTOR = "Native library connector: ";
 
 	@Override
 	public FilterReply decide(ILoggingEvent e) {
-		if (loggerIs(e, WS_CLIENT) && messageIs(e, MESSAGE_CHUNK_ERROR) && levelIs(e, ERROR))
+		if (levelIs(e, ERROR) && loggerIs(e, LOG_WS_CLIENT) && messageIs(e, MSG_CHUNK_ERROR))
 			return DENY;
-		else if (loggerIs(e, WS_CLIENT) && messageIs(e, MESSAGE_CONNECTED) && levelIs(e, INFO))
+		else if (levelIs(e, ERROR) && loggerIs(e, LOG_LATE) && messageStartsWith(e, MSG_PLAYBACK_ERROR))
 			return DENY;
-		else if (loggerIs(e, JDA) && messageIs(e, MESSAGE_LOGIN) && levelIs(e, INFO))
+		else if (levelIs(e, WARN) && loggerIs(e, LOG_OGG_CONTAINER_PROBE) && messageIs(e, MSG_CANT_PARSE_OGG))
 			return DENY;
-		else if (loggerIs(e, JDA) && messageIs(e, MESSAGE_LOADED) && levelIs(e, INFO))
+		else if (levelIs(e, INFO) && loggerIs(e, LOG_WS_CLIENT) && messageIs(e, MSG_CONNECTED))
 			return DENY;
-		else if (loggerIs(e, OGG_CONTAINER_PROBE) && messageIs(e, MESSAGE_CANT_PARSE_OGG) && levelIs(e, WARN))
+		else if (levelIs(e, INFO) && loggerIs(e, LOG_JDA) && messageIs(e, MSG_LOGIN))
 			return DENY;
-		else if (loggerIs(e, LATE) && messageStartsWith(e, MESSAGE_PLAYBACK_ERROR) && levelIs(e, ERROR))
+		else if (levelIs(e, INFO) && loggerIs(e, LOG_JDA) && messageIs(e, MSG_LOADED))
 			return DENY;
-		else if (loggerIs(e, REFLECTIONS) && e.getMessage().startsWith(MESSAGE_REFLECTIONS_TOOK) && levelIs(e, INFO))
+		else if (levelIs(e, INFO) && loggerIs(e, LOG_REFLECTIONS) && messageStartsWith(e, MSG_REFLECTIONS_TOOK))
+			return DENY;
+		else if (levelIs(e, INFO) && loggerIs(e, LOG_YATT) && messageStartsWith(e, MSG_UPDATING_YAT))
+			return DENY;
+		else if (levelIs(e, INFO) && loggerIs(e, LOG_NLL) && messageStartsWith(e, MSG_LOADING_LIBMPG))
+			return DENY;
+		else if (levelIs(e, INFO) && loggerIs(e, LOG_NLL) && messageStartsWith(e, MSG_LOADING_CONNECTOR))
 			return DENY;
 		else
 			return NEUTRAL;
