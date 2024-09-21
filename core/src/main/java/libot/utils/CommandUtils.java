2bf9cd5cf8fd3484dbe7b18a372b900333078006
Marko Zajc <marko@zajc.tel>
2024-09-22 01:44:31 +0200

    [core] Change CommandUtils#findUserOrAuthor arguments

--- core/src/main/java/libot/utils/CommandUtils.java
+++ core/src/main/java/libot/utils/CommandUtils.java
@@ -3,7 +3,9 @@ package libot.utils;
 import static libot.core.Constants.FAILURE;
 import static net.dv8tion.jda.api.utils.MarkdownSanitizer.escape;
 
-import javax.annotation.*;
+import java.util.Optional;
+
+import javax.annotation.Nonnull;
 
 import libot.core.FinderUtils;
 import libot.core.argument.ArgumentList.Argument;
@@ -13,22 +15,28 @@ import net.dv8tion.jda.api.entities.*;
 public final class CommandUtils {
 
 	@Nonnull
-	public static User findUserOrAuthor(@Nonnull CommandContext c, @Nullable Argument arg) {
+	public static User findUserOrAuthor(@Nonnull CommandContext c, @Nonnull Optional<Argument> arg) {
+		return findMemberOrAuthor(c, arg).getUser();
+	}
+
+	@Nonnull
+	public static User findUserOrAuthor(@Nonnull CommandContext c, @Nonnull Argument arg) {
 		return findMemberOrAuthor(c, arg).getUser();
 	}
 
 	@Nonnull
 	@SuppressWarnings("null")
-	public static Member findMemberOrAuthor(@Nonnull CommandContext c, @Nullable Argument arg) {
-		Member target;
-		if (arg != null)
-			target = FinderUtils.findMembers(c, arg.value())
-				.stream()
-				.findFirst()
-				.orElseThrow(() -> c.errorf("Could not find user \"%s\".", FAILURE, escape(arg.value())));
-		else
-			target = c.getMember();
-		return target;
+	public static Member findMemberOrAuthor(@Nonnull CommandContext c, @Nonnull Optional<Argument> arg) {
+		return arg.map(u -> findMemberOrAuthor(c, u)).orElse(c.getMember());
+	}
+
+	@Nonnull
+	@SuppressWarnings("null")
+	public static Member findMemberOrAuthor(@Nonnull CommandContext c, @Nonnull Argument arg) {
+		return FinderUtils.findMembers(c, arg.value())
+			.stream()
+			.findFirst()
+			.orElseThrow(() -> c.errorf("Could not find user \"%s\".", FAILURE, escape(arg.value())));
 	}
 
 	private CommandUtils() {}
